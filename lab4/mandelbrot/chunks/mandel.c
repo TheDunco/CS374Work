3e2aba94fde0d3bcfaa9666111e00df14a1cde68
TheDunco <duncvan@gmail.com>
2021-09-28 15:47:47 -0400

    chunks finally working

--- lab4/mandelbrot/chunks/mandel.c
+++ lab4/mandelbrot/chunks/mandel.c
@@ -50,6 +50,7 @@ int main(int argc, char* argv[])
    const int  WINDOW_WIDTH  = 1200;
    const int WINDOW_SIZE = WINDOW_HEIGHT * WINDOW_WIDTH;
    const double SPACING     = 0.003; // 0.0025
+   double startTime = 0.0, totalTime = 0.0;
 
    // instead of struct make matrix of bools
    bool * myWindow;
@@ -96,29 +97,30 @@ int main(int argc, char* argv[])
     printf("\nSpacing=%lf, center=(%lf,%lf)\n",
             spacing, x_center, y_center);
 */
-   MPE_Open_graphics( &graph, MPI_COMM_WORLD, 
-                     getDisplay(),
-                     -1, -1,
-                     WINDOW_WIDTH, WINDOW_HEIGHT, 0 );
 
+   // allocate memory for the myWindow (size chunkSize * WINDOW_HEIGHT) and finalWindow
    myWindow = (bool *) malloc (sizeof(bool*) * chunkSize * WINDOW_HEIGHT);
    finalWindow = (bool *) malloc (sizeof(bool*) * WINDOW_SIZE);
+
+   if (id == MASTER) {
+      startTime = MPI_Wtime();
+   }
 
    int xCount = 0;
    for (ix = startPos; ix < endPos; ix++)
    {
       for (iy = 0; iy < WINDOW_HEIGHT; iy++)
       {
-          c_real = (ix - 400) * SPACING - x_center;
-          c_imag = (iy - 400) * SPACING - y_center;
-          x = y = 0.0;
-          n = 0;
+         c_real = (ix - 400) * SPACING - x_center;
+         c_imag = (iy - 400) * SPACING - y_center;
+         x = y = 0.0;
+         n = 0;
 
-          while (n < 50 && distance(x, y) < 4.0)
-          {
-             compute(x, y, c_real, c_imag, &x, &y);
-             n++;
-          }
+         while (n < 50 && distance(x, y) < 4.0)
+         {
+            compute(x, y, c_real, c_imag, &x, &y);
+            n++;
+         }
 
          // not in fractal
          if (n < 50) {
@@ -134,6 +136,12 @@ int main(int argc, char* argv[])
    MPI_Gather(myWindow, chunkSize * WINDOW_HEIGHT, MPI_C_BOOL, finalWindow, chunkSize * WINDOW_HEIGHT, MPI_C_BOOL, 0, MPI_COMM_WORLD);
 
    if (id == MASTER) {
+   // only deal with grpahics if we are id 0
+      MPE_Open_graphics( &graph, MPI_COMM_WORLD, 
+                  getDisplay(),
+                  -1, -1,
+                  WINDOW_WIDTH, WINDOW_HEIGHT, 0 );
+
       for (ix = 0; ix < WINDOW_WIDTH; ix++)
       {
          for (iy = 0; iy < WINDOW_HEIGHT; iy++) {
@@ -144,17 +152,19 @@ int main(int argc, char* argv[])
             }
          }
       }
-   }
 
-   // pause until mouse-click so the program doesn't terminate
-   if (id == 0) {
+      totalTime = MPI_Wtime() - startTime;
+
+      printf("%f", totalTime);
+
       printf("\nClick in the window to continue...\n");
       MPE_Get_mouse_press( graph, &ix, &iy, &button );
+      MPE_Close_graphics( &graph );
    }
 
    free(myWindow);
    free(finalWindow);
-   MPE_Close_graphics( &graph );
+
    MPI_Finalize();
    return 0;
 }
